265602c84f8d06648c44f74a3e68128bab9b4b4b
Ryan Chen <chenjunhua1997@gmail.com>
2018-04-17 17:09:49 -0500

    added code for RegisterShopper

--- app/models/RegisteredShopper.java
+++ app/models/RegisteredShopper.java
@@ -1,1 +1,22 @@
 //RegisteredShopper.java
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import io.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+@Entity
+public class Shopper extends Shopper {
+
+    @Id
+    public int id;
+
+    @Constraints.Required
+    public String username;
+
+    @Constraints.Required
+    public String passwordHash;
+}
