9dac9117c2b218a77b1a16c7886131495c9d4e4a
Ryan Chen <chenjunhua1997@gmail.com>
2018-04-23 17:13:42 -0500

    fixed BillingInformation

--- app/models/BillingInformation.java
+++ app/models/BillingInformation.java
@@ -8,19 +8,25 @@ import javax.persistence.*;
 import play.data.format.*;
 import play.data.validation.*;
 
-private int id;
-private int paymentInfoId;
-private String billingAddress;
+@Entity
+public class BillingInformation
+{
+    @Id
+    private int id;
 
-public int getID(){
-    return id;
-}
-public int getPaymentInfoId(){
-    return paymentInfoId
-}
-public String getBillingAddress(){
-    return billingAddress;
-}
-public void setBillingAddress(String billingAddress){
-    this.billingAddress = billingAddress;
+    private int paymentInfoId;
+    private String billingAddress;
+
+    public int getID(){
+        return id;
+    }
+    public int getPaymentInfoId(){
+        return paymentInfoId;
+    }
+    public String getBillingAddress(){
+        return billingAddress;
+    }
+    public void setBillingAddress(String billingAddress){
+        this.billingAddress = billingAddress;
+    }
 }
