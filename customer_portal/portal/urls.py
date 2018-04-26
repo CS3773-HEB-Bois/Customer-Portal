4f3f12032c2ae5fe4861cd27ae810611e8318e86
wolverineisking <logancollier04@gmail.com>
2018-04-26 17:48:33 -0500

    product_id , product cat id and stuff

--- customer_portal/portal/urls.py
+++ customer_portal/portal/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from . import views 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/<int:product_id>', views.product_show, name='alan'),
+    path('products/category/<int:product_category_id>', views.product_category_show,name='cat'),
 ]
