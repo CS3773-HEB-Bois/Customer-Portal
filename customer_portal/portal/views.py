a6962d12460b017a3d2e8d7699f26acb43ce0b14
nallen <allen.nick85@gmail.com>
2018-04-26 17:52:41 -0500

    added cart view

--- customer_portal/portal/views.py
+++ customer_portal/portal/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, ShoppingCart
 import boto3
 
 
@@ -8,3 +8,15 @@ def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'products/index.html', context)
+
+def cart_index(request):
+    #cart_id = request.session.get('shopping_cart_id', '')
+    #if not cart_id:
+    #    return redirect('/')
+    shopping_cart = ShoppingCart.objects.get(pk=1)
+    product_items = shopping_cart.product_items.all()
+
+
+    return render(request, 'cart/index.html', {'product_items': product_items})
+
+
