cfe6e45cf6976b1f07155e7a1a305d094f4049c5
wolverineisking <logancollier04@gmail.com>
2018-04-26 17:48:33 -0500

    product_id , product cat id and stuff

--- customer_portal/portal/views.py
+++ customer_portal/portal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, ProductCategory
 import boto3
 
 
@@ -8,3 +8,15 @@ def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'products/index.html', context)
+
+def product_show(request, product_id):
+    product = Product.objects.get( pk=product_id)
+    context = {'product': product}
+    return render(request, 'products/show.html', context) 
+
+
+def product_category_show(request, product_category_id):
+    cat = ProductCategory.objects.get(pk = product_category_id)
+    #cat.products.all()
+    context = {'cat': cat}
+    return render (request, 'products/showcategory.html',context)
