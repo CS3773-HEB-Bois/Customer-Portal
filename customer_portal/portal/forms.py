02ebaf6fb0123ab4f2360a0607f05d11732f83ca
njgonzales <njgonzales19@gmail.com>
2018-04-27 12:49:13 -0500

    Define basic login logic

--- customer_portal/portal/forms.py
+++ customer_portal/portal/forms.py
@@ -4,3 +4,8 @@ from .models import Product
 
 class AddToCartForm(forms.Form):
     product_id = forms.IntegerField()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=255, min_length=4)
+    password = forms.CharField(max_length=255, min_length=6)
