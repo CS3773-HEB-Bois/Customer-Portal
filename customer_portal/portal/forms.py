90f315a76b831dbbf89fddebeaea6b60348df9c3
Alan Wernick <alan.wernik@gmail.com>
2018-04-30 12:42:17 -0500

    Finish checkout logic

--- customer_portal/portal/forms.py
+++ customer_portal/portal/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, RegisteredShopper, Coupon
+from .models import Product, RegisteredShopper, Coupon, CreditCardInformation
 
 
 class AddToCartForm(forms.Form):
@@ -24,3 +24,27 @@ class DiscountForm(forms.ModelForm):
     class Meta:
         model = Coupon
         fields = ['code']
+
+
+class AddCreditCardForm(forms.ModelForm):
+    class Meta:
+        model = CreditCardInformation
+        fields = ['card_number', 'cvv', 'month', 'year']
+
+
+class OrderForm(forms.Form):
+    billing_address1 = forms.CharField(max_length=255, min_length=4)
+    billing_address2 = forms.CharField(
+        max_length=255, min_length=4, required=False)
+    billing_city = forms.CharField(max_length=255, min_length=4)
+    billing_state = forms.CharField(max_length=255, min_length=2)
+    billing_zip = forms.CharField(max_length=255, min_length=4)
+    payment_option = forms.IntegerField()
+
+    delivery_address1 = forms.CharField(max_length=255, min_length=4)
+    delivery_address2 = forms.CharField(
+        max_length=255, min_length=4, required=False)
+    delivery_city = forms.CharField(max_length=255, min_length=4)
+    delivery_state = forms.CharField(max_length=255, min_length=2)
+    delivery_zip = forms.CharField(max_length=255, min_length=4)
+    delivery_option = forms.CharField(max_length=255, min_length=4)
--- customer_portal/portal/views/checkout.py
+++ customer_portal/portal/views/checkout.py
@@ -1,31 +1,84 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from ..models import ShoppingCart, Order, Coupon
-from ..forms import DiscountForm
+from ..models import ShoppingCart, Order, Coupon, CreditCardInformation, DeliveryPreferences, BillingInformation
+from ..forms import DiscountForm, OrderForm
 
 
 def index(request):
     shopping_cart_id = request.session['shopping_cart_id']
     shopping_cart = ShoppingCart.objects.get(pk=shopping_cart_id)
-    if not shopping_cart.product_items:
+    shopper = shopping_cart.shopper
+    payment_options = CreditCardInformation.objects.filter(shopper=shopper)
+    if not shopping_cart.product_items.exists():
         messages.warning(request, 'You don\'t have any items in your cart')
         return redirect('home')
-    if request.method == "GET":
-        form = DiscountForm()
-        context = {
-            'form': form,
-            'shopping_cart': shopping_cart
-        }
-        return render(request, 'checkout/index.html', context)
-    elif request.method == "POST":
-        if not shopping_cart.order:
-            order = Order()
-            shopping_cart.order = Order()
 
-        context = {
-            'shopping_cart': shopping_cart
-        }
-    return redirect('home')
+    if not hasattr(shopping_cart, 'order'):
+        order = Order()
+        order.shopping_cart = shopping_cart
+        order.save()
+    else:
+        order = shopping_cart.order
+
+    if request.method == "GET":
+        form = OrderForm()
+    elif request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order.save()
+
+            delivery_address = " ".join(
+                [
+                    form.cleaned_data['delivery_address1'],
+                    form.cleaned_data['delivery_address2'],
+                    form.cleaned_data['delivery_city'],
+                    form.cleaned_data['delivery_state'],
+                    form.cleaned_data['delivery_zip'],
+                ]
+            )
+            billing_address = " ".join(
+                [
+                    form.cleaned_data['billing_address1'],
+                    form.cleaned_data['billing_address2'],
+                    form.cleaned_data['billing_city'],
+                    form.cleaned_data['billing_state'],
+                    form.cleaned_data['billing_zip'],
+                ]
+            )
+            delivery_preferences = DeliveryPreferences()
+            delivery_preferences.deliveryAddress = delivery_address
+            delivery_preferences.deliverySpeed = form.cleaned_data['delivery_option']
+            delivery_preferences.order = order
+            delivery_preferences.save()
+            order.delivery_preferences = delivery_preferences
+
+            billing_information = BillingInformation()
+            billing_information.billing_address = billing_address
+            billing_information.payment_information_id = form.cleaned_data['payment_option']
+            billing_information.order = order
+            billing_information.save()
+            order.billing_information = billing_information
+
+            shopping_cart.active = False
+            shopping_cart.save()
+            order.save()
+            messages.success(request, 'We have placed your order! Thanks for shopping with us!')
+            request.session.pop('shopping_cart_id', None)
+            return redirect('home')
+        else:
+            for field in form:
+                print(field)
+                print(field.errors)
+            messages.warning(
+                request, 'Missing necessary information. Please complete all fields and try again')
+
+    context = {
+        'form': form,
+        'order': order,
+        'payment_options': payment_options,
+        'shopping_cart': shopping_cart
+    }
+    return render(request, 'checkout/index.html', context)
 
 
 def add_discount(request):
